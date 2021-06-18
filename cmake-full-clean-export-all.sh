5505a75119e3db846dadac2f25ca965c88d817ee
exqudens <exqudens@gmail.com>
2021-06-17 22:38:29 -0700

    step 23

--- cmake-full-clean-export-all.sh
+++ cmake-full-clean-export-all.sh
@@ -2,10 +2,10 @@
 
 rm -rfv "${USERPROFILE}/.conan/data/exqudens-cpp-eval-conan-strings-lib/" &&\
 rm -rfv "./build/" &&\
-"./cmake-export.sh" "cmd-windows-ninja-msvc-host-x64-target-x64-release-shared" &&\
+"./cmake-export.sh" "cmd-windows-ninja-msvc-release-shared" &&\
 sleep 3s &&\
-"./cmake-export.sh" "cmd-windows-ninja-msvc-host-x64-target-x64-release-static" &&\
+"./cmake-export.sh" "cmd-windows-ninja-msvc-release-static" &&\
 sleep 3s &&\
-"./cmake-export.sh" "cmd-windows-ninja-gcc-host-x64-target-x64-release-shared" &&\
+"./cmake-export.sh" "cmd-windows-ninja-gcc-release-shared" &&\
 sleep 3s &&\
-"./cmake-export.sh" "cmd-windows-ninja-gcc-host-x64-target-x64-release-static"
+"./cmake-export.sh" "cmd-windows-ninja-gcc-release-static"
